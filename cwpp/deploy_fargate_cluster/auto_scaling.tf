e2f43982a2b2bceb692125a17f04573e33b0c99b
davilar18 <davida.avilar@gmail.com>
2022-10-04 09:05:18 -0500

    Update auto_scaling.tf

--- cwpp/deploy_fargate_cluster/auto_scaling.tf
+++ cwpp/deploy_fargate_cluster/auto_scaling.tf
@@ -5,7 +5,7 @@ resource "aws_appautoscaling_target" "target" {
   resource_id        = "service/${aws_ecs_cluster.main.name}/${aws_ecs_service.main.name}"
   scalable_dimension = "ecs:service:DesiredCount"
   min_capacity       = 2
-  max_capacity       = 6
+  max_capacity       = 4
 }
 
 # Automatically scale capacity up by one
