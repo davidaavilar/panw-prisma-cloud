8cae263ab6dd818eb98aa6c67fa64244368c2096
David Avila <33632789+davidaavilar@users.noreply.github.com>
2023-04-21 11:44:30 -0500

    Update main.tf

--- code-security/main.tf
+++ code-security/main.tf
@@ -7,7 +7,7 @@ provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
 }
-
+#new comment
 provider "aws" {
   region     = "us-west-2"
   access_key = "AKIAIOSFODNN7EXAMPLE"
